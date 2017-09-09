c3a54845d593cbc03e8ade90ea7de07b888111ca
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-09-09 15:37:20 -0400

    Add frontend stock quotes

--- backend/src/controllers/frontend/specificbot.js
+++ backend/src/controllers/frontend/specificbot.js
@@ -1,5 +1,6 @@
 'use strict'
 const Bot = require('../../models/bot')
+const request = require('request')
 
 function specificBotController(req, res) {
   let botName = req.params.name
@@ -11,14 +12,36 @@ function specificBotController(req, res) {
       if (bot == null) {
         res.status(404).json({error: 'Bot not found'})
       } else {
-        res.json(bot)
+        fillStockPrices(bot.stocks, stocks => {
+          let objBot = bot.toObject()
+          objBot.stocks = stocks
+          res.json(objBot)
+        })
       }
     }
   }).select({_id: 0, __v: 0})
 }
 
-function fillStockPrices(bot) {
+function fillStockPrices(stocks, cb) {
+  let url = 'http://finance.yahoo.com/d/quotes.csv?f=a&s='
+  stocks.forEach(stock => {
+    url += stock.symbol + '+'
+  })
+  url.slice(0, -1)
 
+  request(url, function (error, response, body) {
+    const priceArray = body.split('\n').slice(0, -1)
+
+    let newStocks = stocks.map((stock, index) => {
+      return {
+        symbol: stock.symbol,
+        name: stock.name,
+        shares: stock.shares,
+        price: parseFloat(priceArray[index])
+      }
+    })
+    cb(newStocks)
+  })
 }
 
 module.exports = specificBotController
